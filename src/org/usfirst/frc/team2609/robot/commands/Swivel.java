2e0fd1ff736b80d8ca35897b56ceb7abf8d8000c
Driver Station <beaverworks2609@gmail.com>
2016-10-27 13:32:30 -0400

    Completed and tuned Swivel auto
    @Author Danny

--- src/org/usfirst/frc/team2609/robot/commands/Swivel.java
+++ src/org/usfirst/frc/team2609/robot/commands/Swivel.java
@@ -9,9 +9,12 @@ public class Swivel extends CommandGroup {
     
     public  Swivel() {
 
-    	addSequential(new DriveEncoder(1335, 0.6, 0));
+    	addSequential(new DriveEncoder(1300, 0.6, 0));
     	addSequential(new GyroTurn(0.3,-90));
     	addSequential(new EncReset());
-    	addSequential(new DriveEncoder(650, 0.6,-90));
+    	addSequential(new DriveEncoder(850, 0.6,-90));
+    	addSequential(new GyroTurn(0.3,0));
+    	addSequential(new EncReset());
+    	addSequential(new DriveEncoder(1540, 0.6, 0));
     }
 }
