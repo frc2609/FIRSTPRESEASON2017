008ad11ed4bbbf60b7ea40f1b336f55e90e94535
Driver Station <beaverworks2609@gmail.com>
2016-10-28 09:47:54 -0400

    Deleted DriveGyro

--- src/org/usfirst/frc/team2609/robot/commands/Swivel.java
+++ src/org/usfirst/frc/team2609/robot/commands/Swivel.java
@@ -12,7 +12,9 @@ public class Swivel extends CommandGroup {
     	addSequential(new DriveEncoder(1300, 0.6, 0));
     	addSequential(new GyroTurn(0.3,-90));
     	addSequential(new EncReset());
-    	addSequential(new DriveEncoder(850, 0.6,-90));
+    	addSequential(new DriveEncoder(500, 0.6,-90));
+    	addSequential(new EncReset());
+    	addSequential(new DriveEncoder(350, 0.6,-90));
     	addSequential(new GyroTurn(0.3,0));
     	addSequential(new EncReset());
     	addSequential(new DriveEncoder(1540, 0.6, 0));
